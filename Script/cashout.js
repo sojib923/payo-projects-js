ca4cbe726ab2d4bd652d8e199138d667acef86ce
sojib923 <sojibahmed2444@gmail.com>
2025-03-13 15:53:29 +0600

    tha commit is cashout.js or main balance from  mainas balance

---
+++ Script/cashout.js
@@ -0,0 +1,20 @@
+document.getElementById("cashout-btn").addEventListener("click",function(event) {
+    event.preventDefault();
+    const pinNumber = document.getElementById("cashout-pin").value;
+    const convertedPin = parseInt(pinNumber);
+
+    const amount = document.getElementById("cashout-amount").value;
+    convertedAmount = parseFloat(amount);
+
+    const mainBalance = document.getElementById("main-balance").innerText;
+    const convertedMainBalance = parseFloat(mainBalance);
+    if (convertedPin === 1234) {
+        const sum = convertedMainBalance - convertedAmount;
+        document.getElementById("main-balance").innerText = sum;
+        console.log(sum)
+
+    }
+    else{
+        alert("Enter Valid Pin")
+    }
+})
